2a8cff7cec7db101824a4ddbcffb847c997cfac7
AmirMahdi Kousheshi <59169445+amkamir82@users.noreply.github.com>
2021-05-02 17:08:34 +0430

    add elastic data

---
+++ Tax Fraud/Tax Fraud/Entity/ElasticData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Entity
+{
+    public class ElasticData
+    {
+        private static List<Node> Models = new List<Node>();
+        static Dictionary<string, List<ElasticData>> allData = new Dictionary<string, List<ElasticData>>();
+        private Dictionary<string, string> _datInformation;
+
+        public ElasticData(string type)
+        {
+            var model = Node.GetNodeByName(type);
+
+            //now we will get the attributes of model
+
+            _datInformation = new Dictionary<string, string>();
+            foreach (var attributesKey in model.Attributes.Keys)
+            {
+                //ToDo
+                _datInformation.Add(attributesKey, "dataInformation");
+            }
+
+            if (allData[type] == null)
+            {
+                allData[type] = new List<ElasticData>();
+            }
+
+            allData[type].Add(this);
+        }
+    }
+}
