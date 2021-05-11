5422ddb0d952f3c74e7a70c3b0dba3b0a32d86c1
Parsa Mohammadian <p.mohammadian@mohaymen.ir>
2021-05-11 20:34:35 +0430

    Add PatternGenerator

--- Tax Fraud/Tax Fraud/Entity/ElasticData.cs
+++ Tax Fraud/Tax Fraud/Entity/ElasticData.cs
@@ -9,10 +9,10 @@ namespace Entity
         static Dictionary<string, List<ElasticData>> allData =
             new Dictionary<string, List<ElasticData>>();
 
-        private static
-            Dictionary<ElasticData, Dictionary<ElasticData, List<ElasticData>>>
-            resultObjects =
-                new Dictionary<ElasticData, Dictionary<ElasticData, List<ElasticData>>>();
+        //private static
+        //    Dictionary<ElasticData, Dictionary<ElasticData, List<ElasticData>>>
+        //    resultObjects =
+        //        new Dictionary<ElasticData, Dictionary<ElasticData, List<ElasticData>>>();
 
         public Dictionary<string, object> data;
 
@@ -39,78 +39,83 @@ namespace Entity
             return keys;
         }
 
-        public static void UpdateDocumentsResults(string firstModelName)
+        //public static void UpdateDocumentsResults(string firstModelName)
+        //{
+        //    foreach (var data in allData[firstModelName])
+        //    {
+        //        resultObjects.Add(data, new Dictionary<ElasticData, List<ElasticData>>());
+        //    }
+        //}
+
+        //public static void UpdateRelationsResults(string modelName)
+        //{
+        //    foreach (var resultObject in resultObjects.Keys)
+        //    {
+        //        foreach (var elasticData in allData[modelName])
+        //        {
+        //            if (elasticData.data["کدملی"].ToString() == resultObject.data["کد ملی"].ToString())
+        //            {
+        //                resultObjects[resultObject].Add(elasticData, new List<ElasticData>());
+        //            }
+        //        }
+        //    }
+        //}
+
+        //public static void UpdateCarsAndHousesResults(string modelName)
+        //{
+        //    foreach (var resultObject in resultObjects.Values)
+        //    {
+        //        foreach (var resultObjectKey in resultObject.Keys)
+        //        {
+        //            foreach (var elasticData in allData[modelName])
+        //            {
+        //                if (elasticData.data["کدملی مالک"].ToString() ==
+        //                    resultObjectKey.data["کدملی عضو خانواده"].ToString())
+        //                {
+        //                    resultObject[resultObjectKey].Add(elasticData);
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
+
+        //public static Dictionary<ElasticData, Dictionary<ElasticData, List<ElasticData>>> GetResultObjects()
+        //{
+        //    Dictionary<ElasticData, Dictionary<ElasticData, List<ElasticData>>> finalResult =
+        //        new Dictionary<ElasticData, Dictionary<ElasticData, List<ElasticData>>>();
+        //    foreach (var resultObjectsValue in resultObjects.Keys)
+        //    {
+        //        Dictionary<ElasticData, List<ElasticData>> value =
+        //            new Dictionary<ElasticData, List<ElasticData>>();
+        //        foreach (var elasticData in resultObjects[resultObjectsValue].Keys)
+        //        {
+        //            List<ElasticData> da = new List<ElasticData>();
+        //            foreach (var elasticData1 in resultObjects[resultObjectsValue][elasticData])
+        //            {
+        //                if (Int64.Parse(elasticData1.data["قیمت معامله"].ToString()) >= 90000000000)
+        //                {
+        //                    da.Add(elasticData1);
+        //                }
+        //            }
+
+        //            if (da.Count != 0)
+        //            {
+        //                value.Add(elasticData, da);
+        //            }
+        //        }
+
+        //        if (value.Count != 0)
+        //        {
+        //            finalResult.Add(resultObjectsValue, value);
+        //        }
+        //    }
+
+        //    return finalResult;
+        //}
+
+        public static void ProcessData()
         {
-            foreach (var data in allData[firstModelName])
-            {
-                resultObjects.Add(data, new Dictionary<ElasticData, List<ElasticData>>());
-            }
-        }
-
-        public static void UpdateRelationsResults(string modelName)
-        {
-            foreach (var resultObject in resultObjects.Keys)
-            {
-                foreach (var elasticData in allData[modelName])
-                {
-                    if (elasticData.data["کدملی"].ToString() == resultObject.data["کد ملی"].ToString())
-                    {
-                        resultObjects[resultObject].Add(elasticData, new List<ElasticData>());
-                    }
-                }
-            }
-        }
-
-        public static void UpdateCarsAndHousesResults(string modelName)
-        {
-            foreach (var resultObject in resultObjects.Values)
-            {
-                foreach (var resultObjectKey in resultObject.Keys)
-                {
-                    foreach (var elasticData in allData[modelName])
-                    {
-                        if (elasticData.data["کدملی مالک"].ToString() ==
-                            resultObjectKey.data["کدملی عضو خانواده"].ToString())
-                        {
-                            resultObject[resultObjectKey].Add(elasticData);
-                        }
-                    }
-                }
-            }
-        }
-
-        public static Dictionary<ElasticData, Dictionary<ElasticData, List<ElasticData>>> GetResultObjects()
-        {
-            Dictionary<ElasticData, Dictionary<ElasticData, List<ElasticData>>> finalResult =
-                new Dictionary<ElasticData, Dictionary<ElasticData, List<ElasticData>>>();
-            foreach (var resultObjectsValue in resultObjects.Keys)
-            {
-                Dictionary<ElasticData, List<ElasticData>> value =
-                    new Dictionary<ElasticData, List<ElasticData>>();
-                foreach (var elasticData in resultObjects[resultObjectsValue].Keys)
-                {
-                    List<ElasticData> da = new List<ElasticData>();
-                    foreach (var elasticData1 in resultObjects[resultObjectsValue][elasticData])
-                    {
-                        if (Int64.Parse(elasticData1.data["قیمت معامله"].ToString()) >= 90000000000)
-                        {
-                            da.Add(elasticData1);
-                        }
-                    }
-
-                    if (da.Count != 0)
-                    {
-                        value.Add(elasticData, da);
-                    }
-                }
-
-                if (value.Count != 0)
-                {
-                    finalResult.Add(resultObjectsValue, value);
-                }
-            }
-
-            return finalResult;
+            //allData["Car"]
         }
     }
 }
---
+++ Tax Fraud/Tax Fraud/Program/PatternGenerator.cs
@@ -0,0 +1,15 @@
+﻿using Entity;
+using Database;
+
+namespace Program
+{
+    class PatternGenerator
+    {
+        private Cache patternCache = new Cache();
+
+        public void Process(ElasticData data)
+        {
+
+        }
+    }
+}
