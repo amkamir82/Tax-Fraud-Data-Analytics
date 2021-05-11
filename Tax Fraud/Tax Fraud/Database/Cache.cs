ffd883206d3b7987f49e5aa1f675a54e4b67e0d2
Parsa Mohammadian <p.mohammadian@mohaymen.ir>
2021-05-11 19:30:32 +0430

    Add cache

---
+++ Tax Fraud/Tax Fraud/Database/Cache.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Database
+{
+    public class Cache
+    {
+        private Dictionary<string, object> _cache;
+        
+        public TRecord GetRecord<TRecord>(string recordId) where TRecord : class
+        {
+            if (_cache.ContainsKey(recordId))
+                return _cache[recordId] as TRecord;
+
+            return default(TRecord);
+        }
+
+        public void CacheRecord<TRecord>(string recordId, TRecord record) where TRecord : class
+        {
+            if (_cache.TryAdd(recordId, record))
+                _cache[recordId] = record;
+        }
+    }
+}
---
+++ Tax Fraud/Tax Fraud/Entity/Pattern.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Entity
+{
+    public class Pattern
+    {
+        public List<ElasticData> Nodes { get; set; }
+        public string Id { get; set; }
+
+        public Pattern(List<ElasticData> nodes, string id)
+        {
+            Nodes = nodes;
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
